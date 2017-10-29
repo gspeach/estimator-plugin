8095a06fc4c506c8d62177f5ba157c9a74eef473
MPham13 <michael.pham.94@gmail.com>
2017-10-29 14:39:15 -0400

    added multi form

--- estimator-plugin.php
+++ estimator-plugin.php
@@ -1,19 +1,19 @@
 <?php 
     /*
-    Plugin Name: Custom Estimation tool
+    Plugin Name: Custom Estimator Tool for Hometown Services LLC
     Plugin URI: 
     Description: Plugin for custom estimation of moving
-    Author: Michael
+    Author: GMU Team 1 Town Movers
     Version: 1.0
     Author URI: 
     */
 
 
-class wp_tool extends WP_Widget {
+class wp_estimatorTool extends WP_Widget {
 
 	// constructor
-	function wp_tool() {
-		parent::WP_Widget(false, $name = __('Estimator Tool', 'wp_tool') );
+	function wp_estimatorTool() {
+		parent::WP_Widget(false, $name = __('Custom Estimation Tool', 'wp_estimatorTool') );
 	}
 
 	// widget form creation
@@ -26,66 +26,47 @@ class wp_tool extends WP_Widget {
 
 	// widget display
 	function widget($args, $instance) {
-		//echo '<form action="index.php" method="post"><input type="submit" value="Click" name="btnSubmit"></form>';
-		echo '
-		<button id="myBtn">Open Modal</button>
+		echo ' 
+<!--Trigger/Open the Modal -->
+<button id="button">Open Modal</button>
 
-<!-- The Modal -->
-<div id="myModal" class="modal">
+<!-- Form 1 Modal -->
+<div id="modal" class="modal" >
 
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close">&times;</span>
-    <p>
-     
-     <form action="">
-		First name: <input type="text" name="FirstName" required><br>
-		Last name: <input type="text" name="LastName" required><br>
-		Email: <input type="text" name="Email"required><br>
-		Phone: <input type="text" name="Phone"required><br>
-        Moving from Zip Code: <input type="text" name="fromZip"required><br>
-        Moving from Home Type: <select name="fromHomeType" required> 
-                                <option></option>
-                                <option>Town Home</option>
-                               </select><br>
-
-       Moving to Zip Code: <input type="text" name="toZip"required><br>
-       Moving to Home Type: <select name="toHomeType"required> 
-                                 <option></option>
-                                <option>Apartment</option>
-                               </select><br>
-	<input id="form-part-1" type="submit" value="Submit">
+<!-- Modal content -->
+<div class="modal-content">
+	<span class="close">&times;</span>
+	<form id="formcontent">
+		First name: <input type="text" id="FirstName"><br>
+		Last name: <input type="text" name="LastName"><br>
+		Email: <input type="text" name="Email"><br>
+		Phone: <input type="text" name="Phone"><br>
+		Moving Date: <input type="date" name="Date"><br>
 	</form>
-
-    </p>
-  </div>
-
+	<button id="submit" type="submit">Submit</button>
 </div>
-		';
+</div>
+		
+		
+		
+		
+		
+		'; //close the echo statement.
 	}
 }
 
 	// register widget
-	add_action('widgets_init', create_function('', 'return register_widget("wp_tool");'));
+	add_action('widgets_init', create_function('', 'return register_widget("wp_estimatorTool");'));
 	
 	function my_script(){
-		wp_enqueue_script('my-script', plugin_dir_url(__FILE__). 'scripts.js',array(), false, true);
+		wp_enqueue_script('my_script', plugin_dir_url(__FILE__). 'scripts.js',array(), false, true);
 	}
 	function my_style(){
-		wp_enqueue_style('my-style', plugin_dir_url(__FILE__). 'style.css');
+		wp_enqueue_style('my_style', plugin_dir_url(__FILE__). 'style.css');
 	}
 	
 	add_action('wp_enqueue_scripts', 'my_script');
 	add_action('wp_enqueue_scripts', 'my_style');
-	
-	/*if($_SERVER['REQUEST_METHOD'] === 'POST'){
-		if (isset($_POST['btnSubmit'])){
-			//submit button has been pressed
-			
-		} 
-	}*/
-	
-	
 ?>
 
 
--- scripts.js
+++ scripts.js
@@ -1,33 +1,47 @@
 // Get the modal
-var modal = document.getElementById('myModal');
+var modal = document.getElementById('modal');
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = document.getElementById("button");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-var submit1 = document.getElementById("form-part-1");
+//Get the submit buttons
+var submitButton = document.getElementById('submit');
 
-// When the user clicks the button, open the modal 
+var formcontent = document.getElementById('formcontent');
+//forms
+var formNum = 1;
+var form2 = "\
+		First name: <input type='text' name='FirstName'><br>\
+";
+
+
+// When the user clicks the button, open the first form 
 btn.onclick = function() {
-    modal.style.display = "block";
+	modal.style.display = "block";
+	formcontent.innerHTML=form1;
 }
 
-// When the user clicks on <span> (x), close the modal
+// When the user clicks on <span> (x), close the first form
 span.onclick = function() {
     modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
+	if (event.target == modal) {
         modal.style.display = "none";
     }
 }
 
-submit1.onclick = function(){
-
-	// alert("Successful submit1");
-	modal.innerHTML = "";
+submit.onclick = function(){
+	if(formNum == 1){
+		formcontent.innerHTML=form2 + document.getElementById('FirstName');
+		formNum = 2;
+	}
+	else{
+		formcontent.innerHTML="";
+	}
 }
