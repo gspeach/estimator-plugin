45047a56e5c89c75e93aa170b6dd49b09b138b2d
Jason Lazarte <jasonlazarte@Jasons-MacBook-Pro.local>
2017-11-12 14:53:26 -0500

    added site colors, close button graphic

--- estimator-plugin.php
+++ estimator-plugin.php
@@ -1,19 +1,19 @@
 <?php 
     /*
-    Plugin Name: Custom Estimation tool
+    Plugin Name: Custom Estimator Tool for Hometown Services LLC
     Plugin URI: 
     Description: Plugin for custom estimation of moving
-    Author: Michael
+    Author: GMU Team 1 Town Movers
     Version: 1.0
     Author URI: 
     */
 
 
-class wp_tool extends WP_Widget {
+class wp_estimatorTool extends WP_Widget {
 
 	// constructor
-	function wp_tool() {
-		parent::WP_Widget(false, $name = __('Estimator Tool', 'wp_tool') );
+	function wp_estimatorTool() {
+		parent::WP_Widget(false, $name = __('Custom Estimation Tool', 'wp_estimatorTool') );
 	}
 
 	// widget form creation
@@ -26,66 +26,94 @@ class wp_tool extends WP_Widget {
 
 	// widget display
 	function widget($args, $instance) {
-		//echo '<form action="index.php" method="post"><input type="submit" value="Click" name="btnSubmit"></form>';
-		echo '
-		<button id="myBtn">Open Modal</button>
+		echo ' 
+<!--Trigger/Open the Modal -->
+<button id="button">Open Modal</button>
 
-<!-- The Modal -->
-<div id="myModal" class="modal">
+<!-- Form Modal -->
+<div id="modal" class="modal" >
 
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close">&times;</span>
-    <p>
-     
-     <form action="">
-		First name: <input type="text" name="FirstName" required><br>
-		Last name: <input type="text" name="LastName" required><br>
-		Email: <input type="text" name="Email"required><br>
-		Phone: <input type="text" name="Phone"required><br>
-        Moving from Zip Code: <input type="text" name="fromZip"required><br>
-        Moving from Home Type: <select name="fromHomeType" required> 
-                                <option></option>
-                                <option>Town Home</option>
-                               </select><br>
-
-       Moving to Zip Code: <input type="text" name="toZip"required><br>
-       Moving to Home Type: <select name="toHomeType"required> 
-                                 <option></option>
-                                <option>Apartment</option>
-                               </select><br>
-	<input id="form-part-1" type="submit" value="Submit">
-	</form>
-
-    </p>
-  </div>
-
+<!-- Modal content -->
+<span class="close">&times;</span>
+<div class="modal-header">
+	FREE MOVING QUOTE
 </div>
-		';
+<div class="modal-content-outer">
+<div class="modal-content">
+	<form id="form1" style="display: none"><br>
+		<input type="text" id="firstname" placeholder="First Name">
+		<input type="text" id="lastname" placeholder="Last Name">
+		<input type="email" id="email" placeholder="Email">
+		<input type="text" id="phone" placeholder="Phone Number">
+		<input type="text" id="date" placeholder="Moving Date" onfocus="(this.type=\'date\')" onblur="(this.type=\'text\')"><br><br>
+		<input id="submitOne" type="button" value="Submit">
+	</form>
+	
+	<form id="form2" style="display: none"><br>
+		Thank you!<br> Please..................<br>Tell us more?<br><br>
+		<input id="submitTwo" type="button" value="Yes">
+		<input id="cancelTwo" type="button" value="No">
+	</form>
+	
+	<form id="form3" style="display: none"><br>
+		Thanks for providing more information! This will help us give you a more accurate quote.
+		<textarea col="50" row="5" id="comments" placeholder="Comments"></textarea>
+		<select id="whomoveing">
+			<option disabled selected>Who is moving?</option>
+			<option value="Owner">Owner</option>
+			<option value="Partial Packing">Partial packing requested</option>
+			<option value="Full Packing">Full packing requested</option>
+			<option value="Unpacking">Unpacking requested</option>
+		</select>
+		<input type="text" id="fromzip" placeholder="Moving from Zipcode">
+		<select id="hometype">
+			<option disabled selected>Home Type</option>
+			<option value="Condo/Apartment">Condo/Apartment</option>
+			<option value="Townhome">Townhome</option>
+			<option value="Single">Single Home</option>
+		</select>
+		<input type="text" id="tozip" placeholder="Moving to Zipcode"><br><br>
+		
+		<div id="rooms">
+		Give us details by room. Click the (+) to add more fields.
+			<input type="text" class="room1" placeholder="Room Name">
+			<select class="room1">
+				<option disabled selected>Furnished:</option>
+				<option value="lightly">Lightly (500lbs)</option>
+				<option value="medium">Medium (1000lbs)</option>
+				<option value="heavy">Heavy (2000lbs)</option>
+			</select>
+			<input type="text" class="room1" placeholder="Additional details..."><br><br>
+		</div>
+		
+		<br><br>
+		<input id="addRooms" type="button" value=" + ">
+		<input id="submitThree" type="button" value="Submit">
+	</form>
+	
+	<form id="form4" style="display: none"><br>
+		Thank you!<br><br>Pleasessess.........<br><br>
+		<input id="submitFour" type="button" value="OK"> 
+	</form>
+</div>
+</div>
+</div>
+		'; //close the echo statement.
 	}
 }
 
 	// register widget
-	add_action('widgets_init', create_function('', 'return register_widget("wp_tool");'));
+	add_action('widgets_init', create_function('', 'return register_widget("wp_estimatorTool");'));
 	
 	function my_script(){
-		wp_enqueue_script('my-script', plugin_dir_url(__FILE__). 'scripts.js',array(), false, true);
+		wp_enqueue_script('my_script', plugin_dir_url(__FILE__). 'scripts.js',array(), false, true);
 	}
 	function my_style(){
-		wp_enqueue_style('my-style', plugin_dir_url(__FILE__). 'style.css');
+		wp_enqueue_style('my_style', plugin_dir_url(__FILE__). 'style.css');
 	}
 	
 	add_action('wp_enqueue_scripts', 'my_script');
 	add_action('wp_enqueue_scripts', 'my_style');
-	
-	/*if($_SERVER['REQUEST_METHOD'] === 'POST'){
-		if (isset($_POST['btnSubmit'])){
-			//submit button has been pressed
-			
-		} 
-	}*/
-	
-	
 ?>
 
 
--- scripts.js
+++ scripts.js
@@ -1,33 +1,141 @@
-// Get the modal
-var modal = document.getElementById('myModal');
+//Checks for support
+if(typeof(Storage) !== "undefined"){
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+	// Get the modal
+	var modal = document.getElementById("modal");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+	// Get the button that opens the modal
+	var btn = document.getElementById("button");
 
-var submit1 = document.getElementById("form-part-1");
+	//Get the button to add room rows
+	var roomButton = document.getElementById("addRooms");
+	
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+	
+	//Forms to be hidden or displayed
+	var form1 = document.getElementById("form1");
+	var form2 = document.getElementById("form2");
+	var form3 = document.getElementById("form3");
+	var form4 = document.getElementById("form4");
+	
+	//Submit buttons to advance the forms or close outerHTML
+	var submitOne = document.getElementById("submitOne");
+	var submitTwo = document.getElementById("submitTwo");
+	var cancelTwo = document.getElementById("cancelTwo");
+	var submitThree = document.getElementById("submitThree");
+	var submitFour = document.getElementById("submitFour");
+	
+	var i = 1;
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+	// When the user clicks the button, open the first form 
+	btn.onclick = function() {
+		modal.style.display = "block";
+		form1.style.display = "block";
+	}
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+	// When the user clicks on <span> (x), close the form
+	span.onclick = function() {
+		modal.style.display = "none";
+	}
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+			form1.style.display = "none";
+			form1.reset();
+			form2.style.display = "none";
+			form3.style.display = "none";
+			form3.reset();
+			form4.style.display = "none";
+		}
+	}
+	
+	//When the user clicks the submit button on form 1, saves the data to local web storage and advances the form
+	submitOne.onclick = function(){
+		localStorage.setItem("firstname", document.getElementById("firstname").value);
+		localStorage.setItem("lastname", document.getElementById("lastname").value);
+		localStorage.setItem("email", document.getElementById("email").value);
+		localStorage.setItem("phone", document.getElementById("phone").value);
+		localStorage.setItem("date", document.getElementById("date").value);
+		
+		form1.style.display="none";
+		form2.style.display="block";
+	}
+	
+	submitTwo.onclick = function(){
+		form2.style.display="none";
+		form3.style.display="block";
+	}
+	
+	cancelTwo.onclick = function(){
+		modal.style.display = "none";
+		form2.style.display = "none";
+	}
+	
+	submitThree.onclick = function(){
+		localStorage.setItem("comments", document.getElementById("comments").value);
+		localStorage.setItem("whomoveing", document.getElementById("whomoveing").value);
+		localStorage.setItem("fromzip", document.getElementById("fromzip").value);
+		localStorage.setItem("hometype", document.getElementById("hometype").value);
+		localStorage.setItem("tozip", document.getElementById("tozip").value);
+		
+		while (document.getElementsByClassName("room"+i).length > 0){
+			var room = document.getElementsByClassName("room"+i);
+			
+			localStorage.setItem("room"+i, JSON.stringify(room));
+			i = i+1;
+		}
+		
+		form3.style.display = "none";
+		form4.style.display = "block";
+	}
+	
+	submitFour.onclick = function(){
+		modal.style.display = "none";
+		form4.style.display = "none";
+	}
+	
+	roomButton.onclick = function(){
+		var container = document.getElementById("rooms");
+		var roomNumber = "room"+i;
+		var input1 = document.createElement("input");
+		var selectList = document.createElement("select");
+		var option1 = document.createElement("option");
+		var option2 = document.createElement("option");
+		var option3 = document.createElement("option");
+		var option4 = document.createElement("option");
+		var input2 = document.createElement("input");
+		
+		input1.type = "text";
+		input1.className = roomNumber;
+		input1.placeholder = "Room Name";
+		container.appendChild(input1);
+		selectList.className = roomNumber;
+		container.appendChild(selectList);
+		option1.disabled = true;
+		option1.selected = true;
+		option1.text = "Furnished:";
+		selectList.appendChild(option1);
+		option2.value = "lightly";
+		option2.text = "Lightly (500lbs)";
+		selectList.appendChild(option2);
+		option3.value = "medium";
+		option3.text = "Medium (1000lbs)";
+		selectList.appendChild(option3);
+		option4.value = "heavy";
+		option4.text = "Heavy (2000lbs)";
+		selectList.appendChild(option4);
+		input2.type="text";
+		input2.className = roomNumber;
+		input2.placeholder = "Addtional details...";
+		container.appendChild(input2);
+		container.appendChild(document.createElement("br"));
+		container.appendChild(document.createElement("br"));
+	}
 
-submit1.onclick = function(){
-
-	// alert("Successful submit1");
-	modal.innerHTML = "";
+//Displays error message in modal content instead of form.
+}else{
+	document.getElementById("modal-content").innerHTML = "Sorry your broswer does not support Web Storage...";
 }
